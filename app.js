83c2163d4a88f43c8fcb7cb348dfb600ec321d8a
phuocnh <56231633+phuocyota@users.noreply.github.com>
2022-04-22 11:52:40 +0700

    fix

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const DBConnection = require('./db/connect')
+const cors = require('cors')
 
 const productRouter = require('./routes/productRouter')
 const authFacebookRouter = require('./routes/authFacebookRouter');
 const authGoogleRouter = require('./routes/authGoogleRouter');
 const app = express();
 
+app.use(cors())
 app.use('/', authFacebookRouter);
 app.use('/', authGoogleRouter);
 app.use('/api/v1/product',productRouter)
